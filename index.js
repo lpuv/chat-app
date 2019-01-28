2d8438a91d2da1d0e6c2a69761d9cb860b9a6c1c
Gamer4Life Puvilland <uyenwontanswer@gmail.com>
2019-01-28 13:38:01 -0800

    Completed this project

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
-const express = require('express');
-const app = express();
-
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+const express = require('express')
+const app = express()
 
 
+//set the template engine ejs
+app.set('view engine', 'ejs')
 
+//middlewares
+app.use(express.static('public'))
+
+
+//routes
 app.get('/', (req, res) => {
-	res.render('index');
-});
+	res.render('index')
+})
 
-server = app.listen(3000);
+//Listen on port 3000
+server = app.listen(3000)
 
-const io = require('socket.io')(server);
 
+
+//socket.io instantiation
+const io = require("socket.io")(server)
+
+
+//listen on every connection
 io.on('connection', (socket) => {
-	console.log('New user connected');
+	console.log('New user connected')
 
-	socket.username = "Anonymous";
+	//default username
+	socket.username = "Anonymous"
 
-	socket.on('change_username', (data) => {
-		socket.username = data.username;
-	});
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
 
-	socket.on('new_message', (data) => {
-		//Broadcast the message
-		io.sockets.emit('new_message', {message : data.message, username : socket.username});
-	});
-});
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+})
